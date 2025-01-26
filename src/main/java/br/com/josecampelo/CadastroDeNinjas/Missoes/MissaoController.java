e3006ed3b7f6f3f5ec66f5eeb9f874b6f75b1c93
Campelo <josecampelojr@gmail.com>
2025-01-25 23:29:15 -0300

    feature: Cria classes de Controller, Service e Repository da entidade Ninja e Missão

---
+++ src/main/java/br/com/josecampelo/CadastroDeNinjas/Missoes/MissaoController.java
@@ -0,0 +1,4 @@
+package br.com.josecampelo.CadastroDeNinjas.Missoes;
+
+public class MissaoController {
+}
---
+++ src/main/java/br/com/josecampelo/CadastroDeNinjas/Missoes/MissaoRepository.java
@@ -0,0 +1,7 @@
+package br.com.josecampelo.CadastroDeNinjas.Missoes;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface MissaoRepository extends JpaRepository<MissaoModel, Long> {
+
+}
