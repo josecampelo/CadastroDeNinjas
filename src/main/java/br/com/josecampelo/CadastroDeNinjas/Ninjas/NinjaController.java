2076552e255fb8071f4c88ed25aee3a2211ec5c7
Campelo <josecampelojr@gmail.com>
2025-01-31 15:51:51 -0300

    feature: Inclui a rota de atualizar o Ninja com Post

--- src/main/java/br/com/josecampelo/CadastroDeNinjas/Ninjas/NinjaController.java
+++ src/main/java/br/com/josecampelo/CadastroDeNinjas/Ninjas/NinjaController.java
@@ -33,9 +33,9 @@ public class NinjaController {
     }
 
     // Alterar dados dos Ninjas (UPDATE)
-    @PutMapping("/alterarID")
-    public String alterarNinjaPorId(){
-        return "Alterar Ninja por ID";
+    @PutMapping("/atualizar/{id}")
+    public NinjaModel atualizarNinja(@PathVariable Long id, @RequestBody NinjaModel ninjaAtualizado){
+        return ninjaService.atualizarNinja(id, ninjaAtualizado);
     }
 
     // Deletar Ninja (DELETE)
